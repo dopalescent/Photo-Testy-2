c8d2564d599ba4a7ab80c0e72be9b5f344cd71c1
dopalescent <skudlgre000@gmail.com>
2023-11-11 20:59:07 -0500

    commented-out almost all of Search.jsx to attempt a dist build/ get heroku to serve the search page

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import {
     Container,
     Col,
@@ -8,98 +8,99 @@ import {
     Row
 } from 'react-bootstrap';
 
-import Auth from '../utils/auth';
-import { searchPhoto } from '../utils/API';
-import { useQuery } from '@apollo/client';
-import { GET_PHOTO } from '../utils/queries';
-import { savePhotoIds, getSavedPhotoIds } from '../utils/localStorage';
+// import Auth from '../utils/auth';
+// import { searchPhoto } from '../utils/API';
+// import { useQuery } from '@apollo/client';
+// import { GET_PHOTO } from '../utils/queries';
+// import { savePhotoIds, getSavedPhotoIds } from '../utils/localStorage';
 
 const SearchPhoto = () => {
-    // create state for holding returned api data
-    const [searchedPhoto, setSearchedPhoto] = useState([]);
-    // create state for holding our search field data
-    const [searchInput, setSearchInput] = useState('');
+    // // create state for holding returned api data
+    // const [searchedPhoto, setSearchedPhoto] = useState([]);
+    // // create state for holding our search field data
+    // const [searchInput, setSearchInput] = useState('');
 
-    // create state to hold saved bookId values
-    const [savedPhotoIds, setSavedPhotoIds] = useState(getSavedPhotoIds());
+    // // create state to hold saved bookId values
+    // const [savedPhotoIds, setSavedPhotoIds] = useState(getSavedPhotoIds());
 
-    // set up useEffect hook to save `savedPhotoIds` list to localStorage on component unmount
-    useEffect(() => {
-        return () => savePhotoIds(savedPhotoIds);
-    });
+    // // set up useEffect hook to save `savedPhotoIds` list to localStorage on component unmount
+    // useEffect(() => {
+    //     return () => savePhotoIds(savedPhotoIds);
+    // });
 
-    // create method to search for photo and set state on form submit
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
+    // // create method to search for photo and set state on form submit
+    // const handleFormSubmit = async (event) => {
+    //     event.preventDefault();
 
-        if (!searchInput) {
-            return false;
-        }
+    //     if (!searchInput) {
+    //         return false;
+    //     }
 
-        try {
-            const response = await searchPhoto(searchInput);
+    //     try {
+    //         const response = await searchPhoto(searchInput);
 
-            if (!response.ok) {
-                throw new Error('something went wrong!');
-            }
+    //         if (!response.ok) {
+    //             throw new Error('something went wrong!');
+    //         }
 
-            const { items } = await response.json();
+    //         const { items } = await response.json();
 
-            const photoData = items.map((photo) => ({
-                photoId: photo.id,
-                username: photo.Info.username || ['No username to display'],
-                title: photo.Info.title,
-                description: photo.volumeInfo.description,
-                imagelink: photo.volumeInfo.imagelink?.thumbnail || '',
-            }));
+    //         const photoData = items.map((photo) => ({
+    //             photoId: photo.id,
+    //             username: photo.Info.username || ['No username to display'],
+    //             title: photo.Info.title,
+    //             description: photo.volumeInfo.description,
+    //             imagelink: photo.volumeInfo.imagelink?.thumbnail || '',
+    //         }));
 
-            setSearchedPhoto(photoData);
-            setSearchInput('');
-        } catch (err) {
-            console.error(err);
-        }
-    };
+    //         setSearchedPhoto(photoData);
+    //         setSearchInput('');
+    //     } catch (err) {
+    //         console.error(err);
+    //     }
+    // };
 
-    // create function to handle saving a photo to our database
-    const handleSavePhoto = async (photoId) => {
-        // find the book in `searchedPhoto` state by the matching id
-        const photoToSave = searchedPhoto.find((photo) => photo.photoId === photoId);
+    // // create function to handle saving a photo to our database
+    // const handleSavePhoto = async (photoId) => {
+    //     // find the book in `searchedPhoto` state by the matching id
+    //     const photoToSave = searchedPhoto.find((photo) => photo.photoId === photoId);
 
-        const [savePhoto] = useQuery(GET_PHOTO);
+    //     const [savePhoto] = useQuery(GET_PHOTO);
 
-        // get token
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+    //     // get token
+    //     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-        if (!token) {
-            return false;
-        }
+    //     if (!token) {
+    //         return false;
+    //     }
 
-        try {
-            const response = await savePhoto(photoToSave, token);
+    //     try {
+    //         const response = await savePhoto(photoToSave, token);
 
-            if (!response.ok) {
-                throw new Error('something went wrong!');
-            }
+    //         if (!response.ok) {
+    //             throw new Error('something went wrong!');
+    //         }
 
-            // if book successfully saves to user's account, save photo id to state
-            setSavedPhotoIds([...savedPhotoIds, photoToSave.photoId]);
-        } catch (err) {
-            console.error(err);
-        }
-    };
+    //         // if book successfully saves to user's account, save photo id to state
+    //         setSavedPhotoIds([...savedPhotoIds, photoToSave.photoId]);
+    //     } catch (err) {
+    //         console.error(err);
+    //     }
+    // };
 
     return (
         <>
             <div className="text-light bg-dark p-5">
                 <Container>
                     <h1>Search for a Photo!</h1>
-                    <Form onSubmit={handleFormSubmit}>
+                    {/* <Form onSubmit={handleFormSubmit}> */}
+                    <Form>
                         <Row>
                             <Col xs={12} md={8}>
                                 <Form.Control
                                     name='searchInput'
-                                    value={searchInput}
-                                    onChange={(e) => setSearchInput(e.target.value)}
+                                    // value={searchInput}
+                                    // onChange={(e) => setSearchInput(e.target.value)}
                                     type='text'
                                     size='lg'
                                     placeholder='Search for a photo'
@@ -117,20 +118,22 @@ const SearchPhoto = () => {
 
             <Container>
                 <h2 className='pt-5'>
-                    {searchedPhoto.length
+                    {/* {searchedPhoto.length
                         ? `Viewing ${searchedPhoto.length} results:`
-                        : 'Search for a photo to begin'}
+                        : 'Search for a photo to begin'} */}
                 </h2>
                 <Row>
-                    {searchedPhoto.map((photo) => {
-                        return (
-                            <Col md="4" key={photo.photoId}>
-                                <Card border='dark'>
-                                    {photo.image ? (
+                    {/* {searchedPhoto.map((photo) => {
+                        return ( */}
+                    {/* <Col md="4" key={photo.photoId}> */}
+                    <Col md="4">
+                        <Card border='dark'>
+                            {/* {photo.image ? (
                                         <Card.Img src={photo.image} alt={`The cover for ${photo.title}`} variant='top' />
-                                    ) : null}
-                                    <Card.Body>
-                                        <Card.Title>{photo.title}</Card.Title>
+                                    ) : null} */}
+                            <Card.Body>
+                                <Card.Title>Testy</Card.Title>
+                                {/* <Card.Title>{photo.title}</Card.Title>
                                         <p className='small'>Usernames: {photo.username}</p>
                                         <Card.Text>{photo.description}</Card.Text>
                                         {Auth.loggedIn() && (
@@ -142,12 +145,12 @@ const SearchPhoto = () => {
                                                     ? 'This book has already been saved!'
                                                     : 'Save this Book!'}
                                             </Button>
-                                        )}
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        );
-                    })}
+                                        )} */}
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    {/* );
+                    })} */}
                 </Row>
             </Container>
         </>
